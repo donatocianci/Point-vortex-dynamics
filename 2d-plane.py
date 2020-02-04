b702d3d826768d880fde411781ab87dc69bbb1e1
Donato Cianci <donato.cianci@gmail.com>
2020-02-04 14:07:58 -0500

    Added comments to 2d-plane.py file.

--- 2d-plane.py
+++ 2d-plane.py
@@ -1,5 +1,14 @@
 
+'''
+This script uses Python to simulate and visualize point vortices in the plane. This version uses the scipy equivalent of
+Matlab's ODE45 solver, which works well unless the system of ODEs becomes stiff, which occurs when two point vortices undergo a
+near collision.
 
+Future to do:
+  * Implement symplectic solver so Hamiltonian is conserved.
+  * Implement bounded planar geometry (either simply-connected or mulitply-connected).
+  * Implement spherical or other surface geometries.
+'''
 
 import numpy as np
 from scipy.integrate import solve_ivp
